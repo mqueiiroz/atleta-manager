1ffc2384641dbaa1e0c1a661a172b4a1a297ac67
mqueiiroz <matheus_queiroz_guerreiro@hotmail.com>
2026-04-13 12:27:04 -0400

    Atualizando a Main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from models import Atleta,Time
+from models import Atleta,Time,Partida
 
 time1 = Time('Ceará')
+time2 = Time('Vasco')
 atleta1 = Atleta('01','Matheus',20,'Defensor')
 time1.adicionar_atleta(atleta1)
 atleta2 = Atleta('02' ,'Mauricio',30,'Defensor')
-time2 = Time('Vasco')
-time1.adicionar_atleta(atleta2)
+time2.adicionar_atleta(atleta2)
+atleta3 = Atleta('03','Caio',21,'Atacante')
+time1.adicionar_atleta(atleta3)
 time1.salvar_dados()
+time2.salvar_dados()
 
+partida = Partida(time1,time2)
+partida.jogar()
